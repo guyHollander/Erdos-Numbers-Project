91cf65fdd8576fabe01b30a9dd3acc42e4d699b8
Guy Hollander <guyholl@gmail.com>
2020-12-06 21:13:59 +0200

    Add Erdos 2 Nodes Reforrmatted The names

---
+++ scripts/parse-erdos2.js
@@ -0,0 +1,19 @@
+import fs from 'fs'
+
+let erdos2Raw = fs.readFileSync('.\\raw_data\\Erdos2.txt',{encoding:'utf-8'}),
+    nodes = fs.readFileSync('.\\data_set\\Nodes', {encoding:'utf-8'}).split('\n').map(s=>s.split(',')),
+    edges = fs.readFileSync('.\\data_set\\Edges')
+
+let erdos2 = erdos2Raw.split('|').map(s => s.split('\r\n'))
+let lastInd = Number(nodes[nodes.length-1][0])
+// console.log(nodes.substring(nodes.lastIndexOf(',')-1,nodes.lastIndexOf(',')))
+// console.log(lastInd)
+let erdos2Nodes = erdos2.map((nodes)=> nodes[0]).map((s)=>s.replace('*','').trim()).map((s)=>s.replace(/\^\d/,'')).
+                        map((s)=>s.split(',')).map((name)=> [name[1], name[0]]).
+                        map((name)=> name[0] ? [name[0].trim().split(' ').join('_'),name[1].split(' ').join('_')] : ['',name[1].split(' ').join('_')]).
+                        map(name=> name.join('_')).
+                        map(name=>[++lastInd,name])
+
+// console.log(erdos2Raw.split('|').map(s => s.split('\r\n')))
+console.log(erdos2Nodes)
+fs.appendFileSync('.\\data_set\\Nodes', '\n' + erdos2Nodes.join('\n'))
--- scripts/parseErdos1.js
+++ scripts/parseErdos1.js
@@ -5,14 +5,15 @@ import path from "path"
 let output = fs.readFileSync(".\\data_set\\Nodes",{encoding:'utf8'}),
     erdos1 = fs.readFileSync(".\\raw_data\\Erdos1.txt",{encoding:'utf8'})
 
-let lastInd = Number(output.substring(output.lastIndexOf(',')-1,output.lastIndexOf(',')))
+let lastInd = 0
 let  nodesArray = erdos1.split('\r\n').map((s)=> s.replace('*','').trim()).
                     map((s)=> [s.substring(s.indexOf(',') +1).trim(), s.substring(0, s.indexOf(',')), ]).
-                    map((person)=> [person[0].replace(' ', '_'), person[1].replace(' ', '_')]).
+                    map((person)=> [person[0].replace(',', '').split(' ').join('_'), person[1].replace(',', '').split(' ').join('_')]).
+                    map(node=>[node[0].replace(/\^\d/,''), node[1].replace(/\^\d/,'')]).
                     map(a=> a.join('_')).
                     map((s)=> [++lastInd,s])
 
 
 fs.appendFileSync(".\\data_set\\Nodes",nodesArray.join('\n'))
-// console.log(nodesArray.join('\n'))
+console.log(nodesArray.join('\n'))
 
