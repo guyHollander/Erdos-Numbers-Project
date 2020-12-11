f2220818215b9b044c37e8acbc5a6790b30a67a9
Guy Hollander <guyholl@gmail.com>
2020-12-11 12:18:22 +0200

    Perliminary version of erdos path

--- erdos_graph/Erdos-Graph.mjs
+++ erdos_graph/Erdos-Graph.mjs
@@ -19,8 +19,47 @@ export class ErdosGraph{
     }
 
     calculateErdosPath(id, name){
-        let node = !id? !name? null : this.nodes.getNodeByName(name) : this.nodes.getNodeById(id)
-        return [node]
+        console.log(id,name)
+        let node = !id? !name ? null : this.nodes.getNodeByName(name) : this.nodes.getNodeById(id)
+        console.log(node);
+        if(!node)
+            return []
+        let bfsQueue = [], visited = {}, dist = -1, isPaulErdos = false , path = []
+        bfsQueue.push(node)
+        while(bfsQueue.length && !isPaulErdos && dist < 5){
+            let currLevel = []
+            dist++
+            while (bfsQueue.length){
+                currLevel.push(bfsQueue.shift())
+            }
+            // console.log(currLevel, bfsQueue);
+            for(const n of currLevel){
+                if(n.id == '0')
+                    isPaulErdos =true
+                
+                let neighbors = n.neighbors
+                for(const neigh of neighbors){
+                    if(!visited[neigh.id]){
+                        visited[neigh.id] = n
+                        bfsQueue.push(neigh)
+                    }
+                }
+            }
+
+        }
+
+        
+        if(!isPaulErdos)
+            return path
+        
+        let curr = this.nodes.getNodeById('0')
+        while(curr.id != node.id){
+            path.push(curr.name)
+            curr = visited[curr.id]
+        }
+        curr.erdosNumber = dist
+        path.push(curr.name)
+        return {path, dist}
     }
 
 
--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,12 @@
 import express from 'express';
-const router = express.Router();
+import {ErdosGraph} from '../erdos_graph/Erdos-Graph.mjs'
+
+const router = express.Router()
+const graph = new ErdosGraph()
 
 /* GET home page. */
 router.get('/erdosPath', function(req, res, next) {
-    
-  res.json({hi:req.query})
+  res.json(graph.calculateErdosPath(null, req.query['name']))
 });
 
 export default router
